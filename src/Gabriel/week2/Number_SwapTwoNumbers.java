50b5aeed66ceeb741e01b924f0b8bd54cd235a04
Gabriel Hernandez <gaboherdez09@gmail.com>
2024-06-01 00:34:29 -0400

    week2

---
+++ src/Gabriel/week2/Number_SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+package Gabriel.week2;
+
+public class Number_SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        int c = a;
+        a = b;
+        b = c;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
