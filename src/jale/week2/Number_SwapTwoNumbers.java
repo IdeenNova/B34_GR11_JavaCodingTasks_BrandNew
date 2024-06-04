085717de01b8562b6d3b8f5ef159884db4e5524b
jaleaker <jalekaya5@gmail.com>
2024-06-03 23:27:54 -0400

    updated

---
+++ src/jale/week2/Number_SwapTwoNumbers.java
@@ -0,0 +1,23 @@
+package jale.week2;
+
+public class Number_SwapTwoNumbers {
+
+    public static void main(String[] args) {
+         int a = 4;
+         int b = 10;
+
+         a = a + b;   // 14
+         b = a - b;   // 4
+         a = a - b;  // 10
+
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+    }
+
+    /*
+    Swap two variables' values without using a third variable
+     */
+
+    //solution 1
+
+}
