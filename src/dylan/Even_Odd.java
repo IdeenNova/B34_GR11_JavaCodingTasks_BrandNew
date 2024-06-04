1ab3cbeaf8f05c4136791b4f41bd903a0301865a
dylansg <dylansg@yahoo.com>
2024-06-04 10:15:32 -0400

    added week 1

---
+++ src/dylan/Even_Odd.java
@@ -0,0 +1,21 @@
+package dylan;
+
+public class Even_Odd {
+    /*
+Write a method which can identifies given number is even or odd.
+ Ex:
+     idenitify(5) -> "Odd"
+     idenitify(6) -> "Even"
+ */
+    public static void main(String[] args) {
+        identify(3);
+    }
+    public static void identify(int num){
+        if (num % 2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
