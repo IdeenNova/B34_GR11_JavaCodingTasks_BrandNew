d58298398bf0760cb0059c5dfdf7272f0ec97759
stefansokic <Cilkasokic@gmail.com>
2024-06-05 19:58:31 -0400

    Homework

--- src/StefanW1_2/week1/FINRA.java
+++ src/StefanW1_2/week1/FINRA.java
@@ -9,21 +9,24 @@ multiple of 5, print "RA" instead of the number. for numbers which are a multipl
 both 3 and 5, print "FINRA" instead of the number.
  */
      public static void main(String[] args) {
-          FINRA(30);
+         int n = 20;
+         int number = 1;
+
+         do {
+
+             if (number % 3 == 0 && number % 5 == 0) {
+                 System.out.print("FINRA ");
+             } else if (number % 3 == 0) {
+                 System.out.print("FIN ");
+             } else if (number % 5 == 0) {
+                 System.out.print("RA ");
+             }else{
+                 System.out.print(number + " ");
+             }
+             number++;
+
+         } while (number <= n);
      }
 
-     private static void FINRA(int num) {
-          String result = "";
-          for (int i = 1; i <= num; i++) { //iterate from 1 to 30
-               if (i % 15 == 0 ) //multiple of both 3 and 5, print "FINRA" instead of the number.
-                    result += "FINRA ";
-               else if (i % 5 == 0) //multiple of 5, print "RA" instead of the number.
-                    result += "RA ";
-               else if (i % 3 == 0)
-                    result += "FIN ";  //multiple of 3, print "FIN" instead of the number.
-               else
-                    result += i + " ";
-          }
-          System.out.println(result);
-     }
+
 }
