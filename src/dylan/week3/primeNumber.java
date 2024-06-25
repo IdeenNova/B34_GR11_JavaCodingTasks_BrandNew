2bf2a3d4d9c240f9c01ac54a6c405c0239ef35eb
dylansg <dylansg@yahoo.com>
2024-06-25 18:00:34 -0400

    added weeks

--- src/dylan/week3/primeNumber.java
+++ src/dylan/week3/primeNumber.java
@@ -4,20 +4,20 @@ public class primeNumber {
      /*
         Write a method that can check if a number is prime or not
      */
-     public static void main(String[] args) {
-         System.out.println(primeNumber(7));
-
-     }
-
-    public static boolean primeNumber(int num) {
-        if (num < 2) {
-            return false;
-        }
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                return false;
+    public static void primeCheck(int n){
+        boolean check = true;
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0 && i != n){
+                check = false;
             }
         }
-        return true;
+        System.out.println(check);
+    }
+
+    public static void main(String[] args) {
+        primeCheck(5);
+        primeCheck(10);
+        primeCheck(13);
+        primeCheck(3000);
     }
 }
--- src/dylan/week3/reverseNegativeNumber.java
+++ src/dylan/week3/reverseNegativeNumber.java
@@ -4,50 +4,19 @@ public class reverseNegativeNumber {
     //Write a return method that can reverse negative number and return it as int
     //ex: given int is ==> -53
     //  the output will be ==> -35
+    public static int revNum(int n){
+        String temp = "";
+        String temp2 = "";
+        temp = String.valueOf(n);
+        for (int i = temp.length()-1; i > 0; i--) {
+            temp2 += temp.charAt(i);
+        }
+        return -1 * Integer.parseInt(temp2);
+    }
+
     public static void main(String[] args) {
-        System.out.println(reverseNum(-6548));
-        System.out.println(reverseNum2(-6548));
-        System.out.println(reverseNum3(-6548));
-
-
+        System.out.println(revNum(-53));
+        System.out.println(revNum(-123123));
     }
-
-    public static int reverseNum(int number) {
-        if (number < 10 && number >= 0) {
-            return number;
-        }
-
-        int result = 0;
-        while (number != 0) {
-            result = result * 10 + number % 10;
-            number /= 10;
-        }
-
-        return result;
-    }
-
-    public static int reverseNum2(int num) {
-        //first approach
-        String str = new StringBuilder("" + num).reverse().toString();
-
-        if (num < 0) {
-            str = "-" + str.substring(0, str.length() - 1);
-        }
-
-        return Integer.parseInt(str);
-    }
-
-    //second approach
-    public static int reverseNum3(int num) {
-        String numAsStr = String.valueOf(num);
-        String[] numInStrArray = numAsStr.split("");
-        //System.out.println(Arrays.toString(numInStrArray));
-        String result = "";
-        for (int i = numInStrArray.length - 1; i > 0; i--) {
-            result += numInStrArray[i];
-        }
-        return -1 * Integer.parseInt(result);
-    }
-
 }
 
