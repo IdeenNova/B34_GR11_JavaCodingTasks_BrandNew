106de6ae1700d1fdcb8ab6c440fe1533f31e69ed
dylansg <dylansg@yahoo.com>
2024-06-25 18:18:56 -0400

    added weeks

--- src/dylan/week4/FrequencyofCharacters.java
+++ src/dylan/week4/FrequencyofCharacters.java
@@ -1,11 +1,29 @@
 package dylan.week4;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class FrequencyofCharacters {
     /*Write a return method that can find the frequency of characters
     Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2*/
+    public static String FrequencyOfChars(String n){
+        String temp = "";
+        for (int i = 0; i < n.length()-1; i++) {
+            int count = 1;
+            char check = n.charAt(i);
+            for (int j = i + 1; j < n.length(); j++) {
+                if (n.charAt(j) == check){
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+            i += count - 1;
+            temp += "" + check + count;
+        }
+        return temp;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(FrequencyOfChars("AAAAAABBBBCDDDDDDD"));
+    }
 
 }
