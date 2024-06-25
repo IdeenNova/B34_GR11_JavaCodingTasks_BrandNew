a0692623e4c9738e06d022bd8c9eb00dbd50b4a2
dylansg <dylansg@yahoo.com>
2024-06-25 19:03:53 -0400

    added weeks

--- src/dylan/week4/RemoveDuplicates.java
+++ src/dylan/week4/RemoveDuplicates.java
@@ -1,11 +1,23 @@
 package dylan.week4;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+
 
 public class RemoveDuplicates {
      /*Write a return method that can remove the duplicated values from String
         Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+    public static String removeDup(String n){
+        String ans = "" + n.charAt(0);
+        for (int i = 1; i < n.length(); i++) {
+            if (!ans.contains("" + n.charAt(i))){
+                ans += n.charAt(i);
+            }
 
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDup("ABAABBBCCCCCCDDDDAASD"));
+    }
 
 }
--- src/dylan/week4/SameLetters.java
+++ src/dylan/week4/SameLetters.java
@@ -7,6 +7,12 @@ public class SameLetters {
     /*Write a return method that check if a string is build out of the same letters as another string.
     Ex:  same("abc",  "cab"); -> true
     same("abc",  "abb"); -> false:*/
+    public static Boolean same(String a, String b){
 
+    }
 
+    public static void main(String[] args) {
+        System.out.println(same("abc", "cab"));
+        System.out.println(same("abc", "abb"));
+    }
 }
