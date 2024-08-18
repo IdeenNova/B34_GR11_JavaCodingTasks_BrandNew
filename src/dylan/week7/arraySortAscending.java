a5e2bc59c5b520c23ca913aa7067220a0d78dfe2
dylansg <dylansg@yahoo.com>
2024-08-18 15:15:13 -0400

    added weeks

--- src/dylan/week7/arraySortAscending.java
+++ src/dylan/week7/arraySortAscending.java
@@ -7,23 +7,23 @@ public class arraySortAscending {
     /*Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
 Ex: int[] arr = {10, 9, 8, 7};
 arr = Sort(arr); ==>{ 7, 8, 9, 10};*/
-    public static int[] sort(int[] array){
-        int[] ans = new int[array.length];
+    public static int[] sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            int min = 0;
             for (int j = 0; j < array.length; j++) {
-                if (array[i] > array[j] && !Arrays.asList(ans).contains(min)){
-                    min = array[j];
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
 
             }
-            ans[i] = min;
         }
-        return ans;
+
+        return array;
     }
 
     public static void main(String[] args) {
-        int[] array = {10, 9, 8, 2, 3, 4};
-        System.out.println(Arrays.toString(sort(array)));
+        int[] arr = {10, 9, 8, 2, 3, 4};
+        System.out.println(Arrays.toString(sort(arr)));
     }
 }
--- src/dylan/week8/MoveZerosToTheEnd.java
+++ src/dylan/week8/MoveZerosToTheEnd.java
@@ -1,10 +1,30 @@
 package dylan.week8;
 
+import java.util.Arrays;
+
 public class MoveZerosToTheEnd {
     /*Write a method that can move all the zeros to  last indexes of the array (Do Not Use Sort Method)
 Ex:
 input:  {1,0,2,0,3,0,4,0};
 output: [1, 2, 3, 4, 0, 0, 0, 0]*/
+    public static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == 0) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
 
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 2, 3, 4};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
 
 }
